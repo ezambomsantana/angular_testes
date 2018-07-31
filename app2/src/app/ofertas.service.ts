2d4fae8c8833ba589490d5179142c3ae90dec3fe
Eduardo <eduardo@doccloud.com.br>
2018-07-31 20:38:17 -0300

    novos desenvolvimentos

--- app2/src/app/ofertas.service.ts
+++ app2/src/app/ofertas.service.ts
@@ -11,7 +11,13 @@ export class OfertasService {
     constructor(private http: HttpClient) {}
 
     public getOfertas(): Promise<Array<Oferta>> {
-        return this.http.get('http://localhost:3000/ofertas')
+        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+            .toPromise()
+            .then((resposta : any) => resposta) 
+    }
+
+    public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
+        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
             .toPromise()
             .then((resposta : any) => resposta) 
     }
